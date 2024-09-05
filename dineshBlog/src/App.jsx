6b1ee37ff124ac8fd1802e314882929cc0e98232
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-05 22:16:50 +0530

    Create Pages and Route

--- dineshBlog/src/App.jsx
+++ dineshBlog/src/App.jsx
@@ -1,10 +1,24 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About";
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
 
+function App() {
   return (
     <div className="">
-      App
+      <Routes>Home
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
