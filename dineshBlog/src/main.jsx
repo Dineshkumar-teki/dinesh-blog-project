387bcb2f186fbefc926af895089d89df25accbec
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-07 18:09:13 +0530

    add redux

--- dineshBlog/src/main.jsx
+++ dineshBlog/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <Provider store={store} >
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </Provider>
 );
--- dineshBlog/src/pages/SignIn.jsx
+++ dineshBlog/src/pages/SignIn.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const {loading, error: errMsg} = useSelector(state => state.user)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default SignIn
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value.trim() });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!formData.password || !formData.email) {
+      return dispatch(signInFailure("Please fill out all fields."));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+      if (res.ok) {
+        dispatch(signInSuccess(data))
+        navigate("/");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+
+  return (
+    <section className="min-h-[90vh] my-10 md:my-0 flex justify-center flex-col md:flex-row items-center">
+      <div className="w-[100%] md:w-[50%] flex justify-center mb-10 md:mb-0">
+        <Link
+          to="/"
+          className="whitespace-nowrap text-4xl font-bold dark:text-white self-center"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Dinesh's
+          </span>
+          Blog
+        </Link>
+      </div>
+      <div className="w-[100%] md:w-[50%] flex flex-col justify-center items-center md:items-start">
+        <form
+          className="flex flex-col gap-4 w-[90%] lg:w-[60%]"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col gap-2">
+            <Label>Your Email</Label>
+            <TextInput
+              type="email"
+              placeholder="name@company.com"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Your Password</Label>
+            <TextInput
+              type="password"
+              placeholder="************"
+              id="password"
+              onChange={handleChange}
+            />
+          </div>
+          <Button
+            type="submit"
+            gradientDuoTone="purpleToPink"
+            disabled={loading}
+          >
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">loading...</span>
+              </>
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+        </form>
+        <div className="flex gap-2 text-sm mt-3">
+          <span>Don't have an account?</span>
+          <Link to="/sign-up" className="text-blue-500">
+            Sign Up
+          </Link>
+        </div>
+        {errMsg && (
+          <Alert className="mt-4 w-[90%] lg:w-[60%]" color="failure">
+            {errMsg}
+          </Alert>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SignIn;
