718d79079c9e6f92c52f0bad2182a5ae4ab4967a
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-07 18:35:38 +0530

    added redux persist

--- dineshBlog/src/main.jsx
+++ dineshBlog/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import { store , persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
   <Provider store={store} >
     <StrictMode>
       <BrowserRouter>
@@ -14,4 +16,5 @@ createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </StrictMode>
   </Provider>
+  </PersistGate>
 );
