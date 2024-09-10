0b0b3001e7bf0315547647ec0bcb4665fad74cde
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-10 22:29:45 +0530

    added about and projects section

--- dineshBlog/src/pages/About.jsx
+++ dineshBlog/src/pages/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="min-h-[90vh] flex justify-center items-center">
+      <div className="max-w-2xl mx-auto p-3 text-center ">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Dinesh's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>........................</p>
+            <p>........................</p>
+            <p>........................</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- dineshBlog/src/pages/Projects.jsx
+++ dineshBlog/src/pages/Projects.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="min-h-[90vh] max-w-2xl mx-auto flex flex-col gap-6 justify-center items-center">
+      <h1 className="text-3xl font-semibold">My Projects</h1>
+      <p className="text-md text-gray-500">
+        Built fun and engaging projects while learning HTML, CSS, JavaScript and
+        React.js
+      </p>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
